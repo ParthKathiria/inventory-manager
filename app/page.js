d1b4475b755740124afae279634b02085e764b21
Parth Kathiria <parth.kathiria169@gmail.com>
2024-08-04 12:07:33 -0400

    updated page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,31 @@
+'use client'
 import Image from "next/image";
+import {useState, useEffect} from 'react';
+import {firestore} from '@/firebase';
+import {Box, Typography} from '@mui/material'
+import { collection } from "firebase/firestore";
 
 export default function Home() {
-  return <>Hello World</>
+  const [inventory, setInventory] = useState([])
+  const [open, setOpen] = useState([])
+  const [itemName, setItemName] = useState['']
+  const updateInventory = async() => {
+    const snapshot = query(collection(firestore, 'inventory'))
+    const docs = await getDocs(snapshot)
+    const inventoryList = []
+    docs.forEach((doc) => {
+      inventoryList.push({
+        name: doc.id,
+        ...doc.data(),
+      })
+    });
+    setInventory[inventoryList]
+  }
+
+  
+  return (
+    <Box>
+      <Typography variant="h1">Inventory Management</Typography>
+    </Box>
+  )
 }
